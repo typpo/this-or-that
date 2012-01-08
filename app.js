0e0cef4a2787cc333c0ea094a6975bd3df909672
Ian <typppo@gmail.com>
2012-01-07 16:01:42 -0800

    small changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(express.cookieParser());
-app.use(express.session({ secret: "keyboard cat" }));
+app.use(express.session({secret: "some key"}));
 
 // Load data
 var groups = [];
@@ -34,22 +34,23 @@ files[1] = clean_file_list('/public/groups/1');
 var numfiles = files[0].length + files[1].length;
 config.MAX_SEEN_CACHE = Math.min(config.MAX_SEEN_CACHE,
     numfiles > config.MAX_SEEN_CACHE_LIMIT
-    ? Math.floor((numfiles - 1) / 2) : numfiles - 1);
+    ? Math.floor((numfiles-1) / 2) : numfiles-1);
 
 // App
 app.get('/', function(req, res) {
-  var grp, imgfile, imgpath = null;
   if (!req.session.last
     || req.session.last.length >= config.MAX_SEEN_CACHE) {
     req.session.last = [];
   }
 
+  var grp, imgfile, imgpath = null;
   do {
     grp = Math.random() < .5 ? 0 : 1;
-    imgfile = files[0][Math.floor(Math.random() * files[0].length)];
+    imgfile = files[grp][Math.floor(Math.random() * files[grp].length)];
     imgpath = '/groups/' + grp + '/' + imgfile;
   } while (req.session.last.indexOf(imgpath) > -1
     || req.session.last_seen == imgpath);
+  console.log('chose ' + imgpath);
 
   lazy_image_resize(__dirname + '/public' + imgpath, function(err, fullpath) {
     // Convert full path to relative path
@@ -82,10 +83,8 @@ function clean_file_list(path) {
 function lazy_image_resize(abs_raw_path, cb) {
   var newpath = abs_raw_path.slice(abs_raw_path, abs_raw_path.lastIndexOf('.'))
     + '-sized' + path.extname(abs_raw_path);
-  console.log('checking ' + abs_raw_path+ ' to ' + newpath);
   path.exists(newpath, function(exists) {
     if (exists) {
-      console.log('found ' + newpath);
       cb(false, newpath);
     }
     else {
